9c174da0af8a24a8660cfdaeb0110b1269ee1e4f
roeechaimo <roee84@gmail.com>
2020-05-08 21:23:44 +0300

    init images data

---
+++ src/appData/images.js
@@ -0,0 +1,43 @@
+let imagesArray = [];
+
+const rabbitImg = { src: "images/rabbit.jpg", id: 1 };
+const burgerImg = { src: "images/burger.jpg", id: 2 };
+const eyeImg = { src: "images/eye.jpg", id: 3 };
+const faceImg = { src: "images/face.jpg", id: 4 };
+const lionImg = { src: "images/lion.jpg", id: 5 };
+const pinguinImg = { src: "images/pinguin.jpg", id: 5 };
+const appleImg = { src: "images/apple.jpg", id: 6 };
+const roseImg = { src: "images/rose.jpg", id: 7 };
+const diamondImg = { src: "images/diamond.jpg", id: 8 };
+const backImg = { src: "images/back.jpg", id: 9 };
+const birdImg = { src: "images/bird.jpg", id: 10 };
+const bodyImg = { src: "images/body.jpg", id: 11 };
+const flowerImg = { src: "images/flower.jpg", id: 12 };
+const handImg = { src: "images/hand.png", id: 12 };
+const heartImg = { src: "images/heart.jpg", id: 13 };
+const mouthImg = { src: "images/mouth.jpg", id: 14 };
+const girlImg = { src: "images/girl.jpg", id: 15 };
+const oldManImg = { src: "images/old-man.jpg", id: 16 };
+
+const blackBackground = { src: "images/black-background.jpg", id: 17 };
+
+imagesArray.push(
+  rabbitImg,
+  burgerImg,
+  eyeImg,
+  faceImg,
+  lionImg,
+  pinguinImg,
+  appleImg,
+  roseImg,
+  diamondImg,
+  backImg,
+  birdImg,
+  bodyImg,
+  flowerImg,
+  handImg,
+  heartImg,
+  mouthImg,
+  girlImg,
+  oldManImg
+);
