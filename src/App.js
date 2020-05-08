03649e622877ab280915614df3669e5ec9ad22fc
roeechaimo <roee84@gmail.com>
2020-05-08 20:40:03 +0300

    init raect router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/">
+              <div>home</div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
