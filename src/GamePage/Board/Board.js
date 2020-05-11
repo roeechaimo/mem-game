cc5e9d478d0f6ef6c41e7bc5ef73648d0b47ba53
roeechaimo <roee84@gmail.com>
2020-05-11 22:01:36 +0300

    adjust cells

--- src/GamePage/Board/Board.js
+++ src/GamePage/Board/Board.js
@@ -1,32 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Board.scss";
 import { IMAGES, BLACK_BACKGROUND } from "./../../appData/images";
 import { HELPERS } from "../../helpers";
 
 function Cell(props) {
-  const { imageUrl } = props;
-  const blackBackground = BLACK_BACKGROUND;
+  const { imageUrl, isActive, onCellClick, index } = props;
+  const blackBackground = BLACK_BACKGROUND.src;
+  const src = isActive ? imageUrl : blackBackground;
+
+  const onClick = index => {
+    typeof onCellClick === "function" && onCellClick(index);
+  };
 
   return (
-    <div class="cell-wrapper">
-      <img src={imageUrl} alt={blackBackground} />
+    <div className="cell-wrapper" onClick={() => onClick(index)}>
+      <img src={src} alt={blackBackground} />
     </div>
   );
 }
 
 function Board(props) {
+  const [activeCell, setActiveCell] = useState(null);
+  const [secondCell, setSecondCell] = useState(null);
+  const [boardImages, setBoardImages] = useState(null);
+  useEffect(() => {});
+
+  const onCellClick = cellIndex => {
+    if (!activeCell || !secondCell) {
+      if (activeCell === null) {
+        return setActiveCell(cellIndex);
+      }
+
+      // TODO
+      if (boardImages[activeCell].id === boardImages[cellIndex].id) {
+        return;
+      }
+
+      return setSecondCell(cellIndex);
+    }
+  };
+
   const { cellNumber = 0 } = props;
-  const shuffeledImages = HELPERS.shuffleArray(IMAGES);
-  let images = [];
-  if (cellNumber > 0) {
-    images = shuffeledImages.slice(0, cellNumber);
+  if (cellNumber > 0 && boardImages === null) {
+    const boardCellNumber = cellNumber / 2;
+    const slicedImages = IMAGES.slice(0, boardCellNumber);
+    const slicedImages_1 = IMAGES.slice(0, boardCellNumber);
+    const imagesDoubbled = [...slicedImages, ...slicedImages_1];
+
+    setBoardImages(HELPERS.shuffleArray(imagesDoubbled));
   }
 
   return (
     <div className="board-container">
-      {images.map(image => (
-        <Cell key={image.id} imageUrl={image.src} />
-      ))}
+      {boardImages &&
+        boardImages.map((image, index) => (
+          <Cell
+            key={index}
+            isActive={activeCell === index || secondCell === index}
+            imageUrl={image.src}
+            onCellClick={cell => onCellClick(cell)}
+            index={index}
+          />
+        ))}
     </div>
   );
 }
