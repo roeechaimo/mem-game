e07a4cefe94d78ff2924532906583a7023c31f11
roeechaimo <roee84@gmail.com>
2020-05-16 21:43:51 +0300

    init react-modal

--- src/GamePage/GamePage.js
+++ src/GamePage/GamePage.js
@@ -1,16 +1,31 @@
 import React from "react";
+import ReactModal from "react-modal";
 import Board from "./Board/Board";
 import Countdown from "./Counter/Countdown";
 import "./gamePage.scss";
 
 function GamePage() {
+  // TODO - fix modal and render on beggining and end of game
+
   return (
     <div className="container">
+      <ReactModal
+        isOpen={false}
+        shouldCloseOnOverlayClick={true}
+        shouldCloseOnEsc={true}
+      >
+        <p>Modal Content</p>
+      </ReactModal>
+
       <Board cellNumber={16} />
 
       <Countdown timeInMinutes={1} />
+
+      <button onClick={()}></button>
     </div>
   );
 }
+
+ReactModal.setAppElement("#root");
 
 export default GamePage;
