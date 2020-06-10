d10acaf2fec31b34d40ffd31514f1484ff8189b0
roeechaimo <roee84@gmail.com>
2020-06-10 21:27:55 +0300

    read firebase config from env file

--- src/firebase/firbase.js
+++ src/firebase/firbase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwJV47vic8HTkjZd0twK2ps1wzGzf5eHk",
-  authDomain: "mem-game-a94ab.firebaseapp.com",
-  databaseURL: "https://mem-game-a94ab.firebaseio.com",
-  projectId: "mem-game-a94ab",
-  storageBucket: "mem-game-a94ab.appspot.com",
-  messagingSenderId: "905350142298",
-  appId: "1:905350142298:web:5df50150408a565dc1fb63",
-  measurementId: "G-ZY35ZRLBLK"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASURMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
